66d8a5d38e15cd541bd3e8301994575e86d13e6a
Denali Balser <denali.balser167@gmail.com>
2019-08-28 18:26:15 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -16,8 +16,8 @@ class Author
     @@post_count+=1
   end
 
-  def add_post_by_title(title)
-    post = Post.new(title)
+  def add_post_by_title(name)
+    post = Post.new(name)
     add_post(post)
     @@post_count+=1
   end
